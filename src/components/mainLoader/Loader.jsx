8be1491a9be25ddbc59fc6261cb8e4a6bc505c2d
Sourav Routh <souravrouth5+leetcode@gmail.com>
2024-05-22 11:19:48 +0530

    added loader for loading screen

---
+++ src/components/mainLoader/Loader.jsx
@@ -0,0 +1,15 @@
+import './Loader.css'
+
+export function Loader(){
+
+    return(
+        <>
+            <div className="loaderWrapper">
+                <div class="centered">
+                    <div class="blob-1"></div>
+                    <div class="blob-2"></div>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/components/mainLoader/Loader2.jsx
@@ -0,0 +1,22 @@
+import './Loader2.css'
+
+export function Loader2(){
+    
+    return(
+        <>
+            <div className="loaderWrapper">
+                <div class="loader">
+                    <span>L</span>
+                    <span>O</span>
+                    <span>A</span>
+                    <span>D</span>
+                    <span>I</span>
+                    <span>N</span>
+                    <span>G</span>
+
+
+                </div>
+            </div>
+        </>
+    )
+}
